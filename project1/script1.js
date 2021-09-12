9cf2b0268f7b7457df582f5eff0417adb877b213
SyedUzair007 <78854168+SyedUzair007@users.noreply.github.com>
2021-09-12 10:29:35 -0700

    new changes
    Signed-off-by: SyedUzair007 <78854168+SyedUzair007@users.noreply.github.com>

--- project1/script1.js
+++ project1/script1.js
@@ -22,14 +22,40 @@ function showSuccess(input){
 
 //function to check if email is valid
 
-function isValidEmail(email){
+function checkEmail(email){
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+   if(re.test(email.value.trim())){
+       showSuccess(email);
+   }else{
+       showError(email,`${getIdField(email)} is not valid!`);
+   }
 }
 
 //function to get the id of the input field
 function getIdField(input){
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+
+//function to check the length and password
+function checkLenght(input,min,max){
+    if(input.value.length < min){
+        showError(input,`${getIdField(input)} needs to be at least ${min} characters`);
+    }
+    else if(input.value.length > max){
+        showError(input,`${getIdField(input)} needs to be at less than ${max} characters`);
+    }
+    else{
+        showSuccess(input);
+    }
+}
+
+//function to pssword match
+
+function checkpassword(input1,input2){
+   if(input1.value!==input2.value){
+       showError(input2,'Password Dont Match!');
+   }
 }
 
 //funtion to check values are valid or not
@@ -49,4 +75,8 @@ form.addEventListener('submit', function(e) {
     console.log(username.value);
 
     checkrequired([username,email,password,confirmpassword]);
+    checkLenght(username,3,10);
+    //checkLenght(password,6,30)
+    checkEmail(email);
+    checkpassword(password,confirmpassword);
 });
